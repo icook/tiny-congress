62bb3422ce7c4496513b6391fe8b54816f48b1f4
Isaac Cook <isaac@simpload.com>
2025-01-31 16:07:59 -0600

    next.js telemetryDisabled

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  telemetry: {
+    telemetryDisabled: true,
+  },
 };
 
 export default nextConfig;
