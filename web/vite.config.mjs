6b0fb4d2b681dad79a18a886639efcc2663640a0
Isaac Cook <isaac@simpload.com>
2025-08-19 06:10:16 -0500

    fix dev ui build

--- web/vite.config.mjs
+++ web/vite.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    strictPort: true,
+    host: process.env.HOST,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
