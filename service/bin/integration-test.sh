4ee65340bbfbbfaf7216765d41e60dae1fcfb961
Isaac Cook <isaac@simpload.com>
2025-12-07 21:31:38 -0600

    Export DATABASE_URL for integration tests

--- service/bin/integration-test.sh
+++ service/bin/integration-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-DATABASE_URL=${DATABASE_URL:-postgres://postgres:postgres@postgres:5432/prioritization}
+export DATABASE_URL="${DATABASE_URL:-postgres://postgres:postgres@postgres:5432/prioritization}"
 cd /usr/src/app
 /usr/local/cargo/bin/cargo test --test integration_tests -- --test-threads=1 --nocapture
