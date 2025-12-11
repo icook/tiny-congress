e10e1fa11ea48923250fb0b2f732e44232312a51
Isaac Cook <isaac@simpload.com>
2025-12-10 22:28:51 -0600

    Add device revoke endpoint and integration test

--- service/src/identity/http/mod.rs
+++ service/src/identity/http/mod.rs
@@ -8,4 +8,8 @@ pub fn router() -> Router {
     Router::new()
         .route("/auth/signup", post(accounts::signup))
         .route("/me/devices/add", post(devices::add_device))
+        .route(
+            "/me/devices/:device_id/revoke",
+            post(devices::revoke_device),
+        )
 }
